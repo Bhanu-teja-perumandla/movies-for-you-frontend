0005902247ece5c8d67839bd8a964773c219ecdc
Bhanu Teja Perumandla <perumandlabhanuteja.gmail.com>
2022-12-29 17:10:10 +0530

    [Srilaya|Bhanu] Style movie cards

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,7 +2,7 @@ import "./Header.css"
 const Header = ()=>{
     return(
         <nav className="header-nav">
-            <h1>MoviesForYou</h1>
+            <h1 className="app-name">MoviesForYou</h1>
             <div className="nav-right-options">
             <h3>Settings</h3>
             <h3>Profile</h3>  
--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -4,9 +4,9 @@ const MovieCard = (props) => {
     return (
         <div className="movie-card">
             <img src={props.poster} alt="movie poster goes here" className="movie-poster"/>
-            <h1>{props.movieName}</h1>
-            <h4>{props.rating}/10</h4>
-            <p>{props.description}</p>
+            <h1 className="movie-title">{props.movieName}</h1>
+            <h4 className="movie-rating">{props.rating}/10</h4>
+            <p className="movie-description">{props.description}</p>
         </div>
     )
 }
