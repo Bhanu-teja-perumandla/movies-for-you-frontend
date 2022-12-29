49aeaa0c8fdbeba68532e76210aedd479f2bb8b5
Srilaya.S <srilaya.s@thoughtworks.com>
2022-12-29 15:56:23 +0530

    [Bhanu|Srilaya] Fetch movies from Tmdb

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,17 +1,53 @@
+import { useEffect, useState } from "react";
 import Header from "../Header/Header";
 import MovieCard from "../MovieCard/MovieCard";
+import keys from "../../access-info"
 import "./Home.css"
 
+
 const Home = () => {
+    
+    const [popularMovies, updatePopularMovies] = useState([]);
+   
+    useEffect(()=>{
+        make_api_call()
+    },[]);
+
+    let make_api_call = async ()=>{
+     try{
+     let raw_response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key="+keys.tmdbApiKey);
+     let response = await raw_response.json();
+     let results = response.results;
+        let movies = results.map((ele)=>{
+          let x = {
+              movieName:ele.original_title,
+              rating:ele.vote_average,
+              poster:"http://image.tmdb.org/t/p/w500/"+ele.poster_path,
+              description:ele.overview,
+
+          }
+          return x;
+        })
+        updatePopularMovies(movies);
+       }catch(e){
+          console.log("error"+e)
+        }
+    }
+
     return(
     <div className = "home-comp">
      <Header></Header>
-     <MovieCard 
-        movieName="MovieName"
-        rating={6}
-        description="Movie was average"
-        poster="./logo512.png"
-     />
+     <div className="movies-list">
+         {popularMovies.map((movie)=>{
+    return (<MovieCard 
+        movieName={movie.movieName}
+        rating={movie.rating}
+        description={movie.description}
+        poster={movie.poster}
+     />)
+         })
+     }
+     </div>
     </div>);
 }
 
