e1fa0d6c00da4c2b4a9992d015a404f326f7c1a9
Bhanu Teja Perumandla <perumandlabhanuteja.gmail.com>
2023-01-01 12:04:29 +0530

    [Srilaya|Bhanu] Add signin and signup pages(not yet working)

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -1,10 +1,35 @@
+import { useDebugValue, useState } from "react";
 import { Link } from "react-router-dom";
+import "./SignIn.css"
 
 const SignIn = ()=>{
-    return(<>
-    <h3>Sign In Page</h3>
-    <Link to="/home">Sign In</Link>
-    </>);
+    const [userDetails, setUserDetails] = useState({
+        email: "",
+        password: ""
+    })
+    console.log(userDetails)
+    function handleSubmit(event) {
+        event.preventDefault()
+        let newUser = {
+            email : event.target.email.value,
+            password : event.target.password.value,
+        }
+        setUserDetails(()=>{
+            return newUser
+        })
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className="signin-form">
+             <input type="email" required={true} placeholder="Email" id="email" className="form-input" />
+             <input type="password" required={true} placeholder="Password" id="password" className="form-input"/>
+             <button className="signin-btn">Sign In</button>
+             <p className="signup-text">Don't have an account? <Link to="/signUp" className="signup-link">Sign Up</Link></p>
+        </form>
+    );
 }
 
 export default SignIn;
+
+// {/* <h3>Sign In Page</h3>
+// <Link to="/home">Sign In</Link> */}
--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -1,10 +1,48 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import "./SignUp.css"
 
 const SignUp = ()=>{
-    return(<>
-    <h3>Sign Up Page</h3>
-    <Link to="/home">Sign Up</Link>
-    </>);
+    const [userDetails, setUserDetails] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
+    const [isSignedUp, setIsSignedUp] = useState(false)
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log(userDetails)
+        let users = JSON.parse(localStorage.getItem("users")) || []
+        users.push(userDetails)
+        localStorage.setItem("users",JSON.stringify(users))
+        setIsSignedUp(true)
+    }
+    function handleChange(event) {
+        let id = event.target.id
+        let value = event.target.value
+        setUserDetails(prevDetails => {
+            return {
+                ...prevDetails,
+                [id] : value
+            }
+        })
+    }
+    return(
+        <>
+            {isSignedUp && <Navigate to="/"/>}
+            <form className="signup-form" onSubmit={handleSubmit}>
+                <input type="text" required={true} placeholder="First Name" id="firstName" className="form-input" onChange={handleChange}/>
+                <input type="text" required={true} placeholder="Last Name" id="lastName" className="form-input" onChange={handleChange}/>
+                <input type="email" required={true} placeholder="Email" id="email" className="form-input" onChange={handleChange}/>
+                <input type="password" required={true} placeholder="Password" id="password" className="form-input" onChange={handleChange}/>
+                <input type="password" required={true} placeholder="Confirm Password" id="confirmPassword" className="form-input" onChange={handleChange}/>
+                <button className="signup-btn">Sign Up</button>
+            </form>
+        </>
+    );
 }
 
 export default SignUp;
