ef88c2c5f2e7859b76dc9c0a79a85a6649fd68d5
Srilaya.S <srilayasangannagari@gmail.com>
2023-01-02 10:25:39 +0530

    [Bhanu|Srilaya] Add sign in functionality

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -1,35 +1,43 @@
-import { useDebugValue, useState } from "react";
-import { Link } from "react-router-dom";
+import {useState} from "react";
+import { Link, Navigate } from "react-router-dom";
 import "./SignIn.css"
 
 const SignIn = ()=>{
-    const [userDetails, setUserDetails] = useState({
-        email: "",
-        password: ""
-    })
-    console.log(userDetails)
+    const [isSignedIn, setIsSignedIn] = useState(false)
+    const [message, setMessage] = useState("")
     function handleSubmit(event) {
         event.preventDefault()
         let newUser = {
             email : event.target.email.value,
             password : event.target.password.value,
         }
-        setUserDetails(()=>{
-            return newUser
-        })
+        let users = JSON.parse(localStorage.getItem("users"))||[]
+        let message  = "Account not found"
+        users.map(user=>{
+           if(user.email===newUser.email && user.password===newUser.password){
+              message = ""
+              setIsSignedIn(true)
+           }
+           else if(user.email===newUser.email ){
+               message = "password incorrect"
+           }
+        });
+        setMessage(message)
+
     }
 
     return (
+       <>
+        {isSignedIn && <Navigate to="/"/>}
         <form onSubmit={handleSubmit} className="signin-form">
              <input type="email" required={true} placeholder="Email" id="email" className="form-input" />
              <input type="password" required={true} placeholder="Password" id="password" className="form-input"/>
              <button className="signin-btn">Sign In</button>
+             {message && <p className="invalid-message">{message}</p>}
              <p className="signup-text">Don't have an account? <Link to="/signUp" className="signup-link">Sign Up</Link></p>
         </form>
+        </> 
     );
 }
 
 export default SignIn;
-
-// {/* <h3>Sign In Page</h3>
-// <Link to="/home">Sign In</Link> */}
