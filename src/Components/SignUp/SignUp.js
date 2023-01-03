f0b2c154a3e56079f7afb0ca71f1c9c2d6dfda99
Bhanu Teja Perumandla <perumandlabhanuteja.gmail.com>
2023-01-03 09:50:49 +0530

    [Srilaya|Bhanu] add email and password validation for signup

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -11,14 +11,25 @@ const SignUp = ()=>{
         confirmPassword: ""
     })
     const [isSignedUp, setIsSignedUp] = useState(false)
+    const [message, setMessage] = useState(null)
 
     function handleSubmit(event) {
         event.preventDefault()
         console.log(userDetails)
+        let message = ""
         let users = JSON.parse(localStorage.getItem("users")) || []
-        users.push(userDetails)
-        localStorage.setItem("users",JSON.stringify(users))
-        setIsSignedUp(true)
+        if (users.find(user=> user.email === userDetails.email)) {
+            message="Email already exists"
+        }
+        else if (userDetails.password !== userDetails.confirmPassword) {
+            message="Passwords do not match"
+        }
+        setMessage(message)
+        if (!message) {
+            users.push(userDetails)
+            localStorage.setItem("users",JSON.stringify(users))
+            setIsSignedUp(true)
+        }
     }
     function handleChange(event) {
         let id = event.target.id
@@ -40,6 +51,7 @@ const SignUp = ()=>{
                 <input type="password" required={true} placeholder="Password" id="password" className="form-input" onChange={handleChange}/>
                 <input type="password" required={true} placeholder="Confirm Password" id="confirmPassword" className="form-input" onChange={handleChange}/>
                 <button className="signup-btn">Sign Up</button>
+                {message && <p className="invalid-message">{message}</p>}
                 <p className="signin-text">Already have an account? <Link to="/signIn" className="signin-link">Sign In</Link></p>
             </form>
         </>
